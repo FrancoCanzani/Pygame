90bab55eb9b0ab277fdbb9a5dcccf624cebf0b3f
Franco <francocanzani@gmail.com>
2021-04-01 13:55:50 -0300

    Nuevos cambios en el juego, villano añadido

--- Space Invaders/spaceInvaders.py
+++ Space Invaders/spaceInvaders.py
@@ -15,6 +15,13 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("Nave espacial.png")
 playerX = 370 # Donde va a estar situado
 playerY = 480
+playerX_Change = 0
+
+# Enemigo
+playerImg = pygame.image.load("alien.png")
+playerX = 370 # Donde va a estar situado
+playerY = 480
+playerX_Change = 0
 
 def player():
     screen.blit(playerImg, (playerX, playerY))
@@ -23,22 +30,30 @@ def player():
 # Event es algo que sucede en el juego
 running = True
 while running:
+
+    screen.fill((0, 0, 0)) # RGB color para el fondo
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
 # Definimos que van a hacer las teclas en el juego
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.KEY_LEFT:
-            print("Left arrow is pressed.")
-        if event.key == pygame.KEY_RIGHT:
-            print("Right arrow is pressed.")
-    if event.type == pygame.KEYUP:
-        if event.key == pygame. KEY_LEFT or pygame.K_RIGHT:
-            print("Keystoke has been released.")
+        if event.type == pygame.KEYDOWN:
+            print("A keystoke is pressed")
+            if event.key == pygame.K_LEFT:
+                playerX_Change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_Change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_Change = 0
 
+    playerX += playerX_Change # Hace que se vaya modificando la posicion de la nave a medida que tocamos las felchas.
 
-    screen.fill((0, 0, 0)) # RGB color para el fondo
+# Delimitamos los limites de la nave para que no se pase afuera de la pantalla
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
     # Poner el player despues de la screen porque sino estaria dibujado abajo de la pantalla y no arriba
     player()
     pygame.display.update() # Actualizamos para aplicar lo que pasa en el loop a la pantalla
